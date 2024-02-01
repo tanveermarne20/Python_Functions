f9bbe49acda8f84c5c8b3d93bd2d53446e968e1e
Tanvir Marne <130769036+tanveermarne20@users.noreply.github.com>
2024-02-01 19:06:12 +0530

    odd even added

---
+++ If_else_statements/Even_odd.py
@@ -0,0 +1,8 @@
+num=int(input("Enter the number"))
+
+if num%2==0:
+    print(f"{num} is even number")
+elif num%2 !=0:
+    print(f"{num} is odd number")
+else:
+    print(f"{num} is neither even nor odd")
