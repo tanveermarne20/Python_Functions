1b9f28bc21befdde08ad8f74cb94d1b26c9a1789
Tanvir Marne <130769036+tanveermarne20@users.noreply.github.com>
2024-02-02 09:43:54 +0530

    marks of 3 sub

---
+++ If_else_statements/Marks_of_3_subjects.py
@@ -0,0 +1,11 @@
+math=float(input("Enter marks of math"))
+geo=float(input("Enter marks of geo"))
+chem=float(input("Enter marks of chem"))
+
+total_marks=math+geo+chem
+percentage=(total_marks/300)*100
+
+if percentage<60:
+    print(f"Har luck ,next time    {percentage}")
+elif percentage>=60:
+    print(f"passed {percentage}")
